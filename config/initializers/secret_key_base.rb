48577334134311ff50d5da3574d75ec1fbbc1695
Tomas Valent <equivalent@eq8.eu>
2021-01-10 17:23:55 +0100

    test env variables on production

--- config/initializers/secret_key_base.rb
+++ config/initializers/secret_key_base.rb
@@ -1,4 +1,3 @@
-ENV['SECRET_KEY_BASE'] = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 Rails.application.configure do
   config.require_master_key = false
   config.read_encrypted_secrets = false
